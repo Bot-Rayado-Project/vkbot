e61dffd44fd76b5a3ba627b3d6265ef152e45286
Ivan <ice@gmail.com>
2021-12-01 14:06:17 -0800

    make some changes

--- whataweek.py
+++ whataweek.py
@@ -6,4 +6,5 @@ def get_week():
     url = 'https://whataweek.ru/'
     responce = requests.get(url)
     soup = BeautifulSoup(responce.text, 'lxml')
-    return ((str(soup.find_all("div", {"class": "uppercase typo-h2"})).replace('[<div class="uppercase typo-h2">', '')).replace('</div>]', ''))
+    #return ((str(soup.find_all("div", {"class": "uppercase typo-h2"})).replace('[<div class="uppercase typo-h2">', '')).replace('</div>]', ''))
+    return 'нечетная'
