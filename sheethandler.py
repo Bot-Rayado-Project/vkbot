101447a26c6061eba1645f1afecf0338e6e0e113
Ivan <ice@gmail.com>
2021-12-01 10:50:57 -0800

    Add raspisanie

--- sheethandler.py
+++ sheethandler.py
@@ -2,7 +2,7 @@ import download
 import xlrd
 import whataweek
 
-global groups
+global groups, columns
 groups_id = {'бфи2101': 3,
              'бфи2102': 4,
              'бвт2101': 5,
@@ -35,13 +35,23 @@ def get_sheet():
 
 def print_schedule(start, end):
     global groups_id, group
-    schedule = '⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻\n'
-    schedule += time[k] + '\n'
+    schedule = [] #str(sheet.cell_value(i, groups_id[group]))
+    schedule.append(time[k])
     for i in range(start, end + 1):
-        if str(sheet.cell_value(i, groups_id[group])) != '':
-            schedule += str(sheet.cell_value(i, groups_id[group]) + '\n')
-    if k == 4:
-        schedule += '⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻\n'
+        temp = str(sheet.cell_value(i, groups_id[group]))
+        if temp != '':
+            if 'дистанционно' in temp or 'Дистанционно' in temp:
+                schedule.append(temp[len(temp) - 12 : len(temp)])
+            else:
+                schedule.append(temp)
+    if len(schedule) == 4:
+        temp = schedule[1] + ' ' + schedule[2]
+        temp_dist = schedule[3]
+        schedule.pop(1)
+        schedule.pop(1)
+        schedule.pop(1)
+        schedule.append(temp)
+        schedule.append(temp_dist)
     return schedule
 
 
@@ -73,46 +83,65 @@ def pred_print(day, i):
     start, end = calculation(day, i)
     return print_schedule(start, end)
 
+def table_ui(stroka1):
+    global stroka
+    
+    print_group = '⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻\n' + 'Группа: ' + group.upper() + '\n' \
+    + 'День недели: ' + day.capitalize() + '\n' + 'Неделя: ' + whataweek.get_week().capitalize() + '\n' \
+    + '⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻\n'
+
+    for i in range(len(stroka)):
+        print_group += '\n' + 'Пара №' + str(i + 1) + ' (' + str(stroka[i][0]) + ')' + '\n'
+        if len(stroka[i]) > 2:
+            if 'ФИЗИЧЕСКАЯ' in stroka[i][1]:
+                print_group += stroka[i][1][1:len(stroka[i][1])] + '\n'
+            else:
+                print_group += stroka[i][1] + '\n'
+            print_group += 'Кабинет: ' + stroka[i][2] + '\n' + '\n' + '⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻\n'
+        else:
+            print_group += 'Пары нет' + '\n' + '\n' + '⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻\n'
+    return print_group
+
 
 def get_schedule(day_of_week, group_input):
 
-    global sheet, time, k, stroka, groups, group
+    global sheet, time, k, stroka, groups, group, day
     group = group_input
-    stroka = ''
+    day = day_of_week
+    stroka = []
     k = 0
-    time = ['9-30', '11-20', '13-10', '15-25', '17-15']
+    time = ['09-30', '11-20', '13-10', '15-25', '17-15']
     sheet = get_sheet()
 
     days = [11, 31, 51, 71, 91, 111]
 
     if day_of_week == 'понедельник':
         for i in range(5):
-            stroka += pred_print(days[0], i)
+            stroka.append(pred_print(days[0], i))
             k += 1
 
     if day_of_week == 'вторник':
         for i in range(5):
-            stroka += pred_print(days[1], i)
+            stroka.append(pred_print(days[1], i))
             k += 1
 
     if day_of_week == 'среда':
         for i in range(5):
-            stroka += pred_print(days[2], i)
+            stroka.append(pred_print(days[2], i))
             k += 1
 
     if day_of_week == 'четверг':
         for i in range(5):
-            stroka += pred_print(days[3], i)
+            stroka.append(pred_print(days[3], i))
             k += 1
 
     if day_of_week == 'пятница':
         for i in range(5):
-            stroka += pred_print(days[4], i)
+            stroka.append(pred_print(days[4], i))
             k += 1
 
     if day_of_week == 'суббота':
         for i in range(5):
-            stroka += pred_print(days[5], i)
+            stroka.append(pred_print(days[5], i))
             k += 1
-
-    return stroka
+    return table_ui(stroka)
