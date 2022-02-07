7dfbfa38f46ae06ed970f2d0df0b78aa4d99b031
3xiced <3xiced@gmail.com>
2022-02-07 19:44:41 +0300

    reworked download function

--- download.py
+++ download.py
@@ -4,40 +4,11 @@ import os
 import shutil
 import re
 
-is_xls, is_downloaded = None, None
-
 
 def download_sheet():
-    global is_xls, is_downloaded
-    url = 'https://mtuci.ru/time-table/'
-    responce = requests.get(url)
-    soup = BeautifulSoup(responce.text, 'lxml')
-    if os.path.exists("temp"):
-        shutil.rmtree("temp")
-        os.mkdir("temp")
-    else:
-        os.mkdir("temp")
-    print('Entry point')
-    for line in soup.find_all('span'):
-        __line__ = (((str(line).replace(' ', '')).lower()).replace(
-            '<span>', '')).replace('</span>', '')
-        if 'расписаниезанятий' in __line__ and '09.03.01' in __line__ and '2курс' in __line__:
-            print((str(line).replace('<span>', '')).replace('</span>', ''))
-            if '.xlsx' in (str(line).replace('<span>', '')).replace('</span>', ''):
-                file = open('temp/table_xlsx.xlsx', 'wb')
-                is_xls = False
-                print('XLSX')
-            else:
-                file = open('temp/table_xls.xls', 'wb')
-                is_xls = True
-                print('XLS')
-            ufr = requests.get("https://mtuci.ru/time-table/files/{0}".format(re.sub(
-                "^\s+|\n|\r|\s+$", '', (str(line).replace('<span>', '')).replace('</span>', ''))))
-            file.write(ufr.content)
-            file.close()
-            is_downloaded = True
-            print('FILE WRITTEN!')
-            break
+    link = "https://mtuci.ru/upload/iblock/f9c/1-kurs-_IT_09.03.01-Informatika-i-vychislitelnaya-tekhnika.xlsx"
+    file = open('table.xlsx', 'wb')
+    file.write(requests.get(link).content)
 
 
 if __name__ == '__main__':
