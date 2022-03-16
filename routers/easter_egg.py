66711277bd0d9ce5a4f28dd60aee07bf24e3bb4f
3xiced <3xiced@gmail.com>
2022-03-16 15:12:37 +0300

    Fixed 'was never awaited' error.

--- routers/easter_egg.py
+++ routers/easter_egg.py
@@ -12,5 +12,5 @@ easter_egg_router = DefaultRouter()
 @simple_bot_message_handler(easter_egg_router, TextFilter("дима"))
 @database_handler()
 async def easteregg(event: SimpleBotEvent) -> None:
-    photo = get_photo_from_link(event, "https://memes.co.in/memes/update/uploads/2021/12/InShot_20211209_222013681-1024x1024.jpg")
+    photo = await get_photo_from_link(event, "https://memes.co.in/memes/update/uploads/2021/12/InShot_20211209_222013681-1024x1024.jpg")
     await event.answer(message='?', keyboard=menu_kb.START_KB.get_keyboard(), attachment=photo)
