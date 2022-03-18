0a508687d9e259d9e21b18079fe31be625d77346
3xiced <3xiced@gmail.com>
2022-03-18 13:42:59 +0300

    Полу сделаный полу рабочий код, херня какая то, только хуже сделал хз

--- entry.py
+++ entry.py
@@ -1,11 +1,44 @@
 import sqlite3
 import sys
 import logging
+from utils.exceptions import keyboard_interrupt
 from vkwave.bots import SimpleLongPollBot
 from utils.terminal_codes import print_info, print_error
 from utils.settings import Settings
 
 settings = Settings()
+
+
+class InitializeComponent:
+    def __init__(self, routers_list: list, token: str = None, group_id: str = None) -> None:
+        '''Импортирует все нужное для запуска бота и инициализирует его класс в ручном либо автоматическом режиме.
+        Для переход в ручной режим введите токены и id группы в поля token, group_id'''
+        print_info("Waiting for application startup...")
+        if token == None and group_id == None:
+            print_info("Creating bot instance in automatic mode...")
+            self.bot = SimpleLongPollBot(settings.GET_API_TOKEN(), settings.GET_GROUP_ID())
+            for router in routers_list:
+                self.bot.dispatcher.add_router(router)
+        else:
+            print_info("Creating bot instance in manual mode...")
+            self.bot = SimpleLongPollBot(token, group_id)
+            for router in routers_list:
+                self.bot.dispatcher.add_router(router)
+        # Установка обработчка прерывания с клавиатуры
+        self.__set_keyboard_interrupt()
+        # Установка логирования
+        self.__set_logging()
+        print_info("Application startup complete.")
+        print_info("Started listening for messages...")
+
+    def run(self) -> None:
+        self.bot.run_forever()
+
+    def __set_keyboard_interrupt(self) -> None:
+        sys.excepthook = keyboard_interrupt
+
+    def __set_logging(self) -> None:
+        logging.basicConfig(filename="errors.log", level=logging.ERROR)
 
 
 def set_up_connection_with_db(data_base_name: str) -> tuple | None:
@@ -16,21 +49,3 @@ def set_up_connection_with_db(data_base_name: str) -> tuple | None:
     except sqlite3.Error:
         print_error("Database connection failure.")
         exit()
-
-
-def keyboard_interrupt(exctype, value, traceback):
-    if exctype == KeyboardInterrupt:
-        print_info("Keyboard interrupt has been detected.")
-        print_info("Shutting down.")
-        exit()
-    else:
-        sys.__excepthook__(exctype, value, traceback)
-
-
-def InitializeComponent() -> SimpleLongPollBot:
-    ALLOWED_USER_IDS, API_TOKEN, GROUP_ID = settings.GET_ALL_VARIABLES()
-    # Установка логирования
-    logging.basicConfig(filename="logs.log", level=logging.ERROR)
-    # Установка обработчика ошибок
-    sys.excepthook = keyboard_interrupt
-    return SimpleLongPollBot(API_TOKEN, GROUP_ID)
--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,2 +1,16 @@
+import sys
+
+from utils.terminal_codes import print_info
+
+
 class SearchException(Exception):
     pass
+
+
+def keyboard_interrupt(exctype, value, traceback):
+    if exctype == KeyboardInterrupt:
+        print_info("Keyboard interrupt has been detected.")
+        print_info("Shutting down.")
+        exit()
+    else:
+        sys.__excepthook__(exctype, value, traceback)
