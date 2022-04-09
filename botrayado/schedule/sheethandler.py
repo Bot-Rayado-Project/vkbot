790ca5bf103dd18a2f4cb6d22a904c706f54ba03
darttusin <74204840+darttusin@users.noreply.github.com>
2022-04-10 01:33:31 +0300

    Update sheethandler.py

--- botrayado/schedule/sheethandler.py
+++ botrayado/schedule/sheethandler.py
@@ -14,27 +14,29 @@ async def aiohttp_fetch(url: str) -> str:
 
 async def print_schedule(day_type: str, group: str) -> str:
 
-    day_time_utc = datetime.weekday(datetime.today().utcnow() + timedelta(hours=3))
-
+    day_time_utc = datetime.weekday(
+        datetime.today().utcnow() + timedelta(hours=3))
     if day_type == 'завтра':
         day_time_utc += 1
     if day_time_utc == 6:
         return 'Занятий нет'
+    if day_time_utc == 7:
+        day_time_utc = 0
 
     week_checked = await get_week()
     even = True if week_checked == 'четная' else False
+    if week_checked == 'четная' and day_type == 'завтра' and day_time_utc == 0:
+        even = False
+        week_checked = 'нечетная'
+    elif week_checked == 'нечетная' and day_type == 'завтра' and day_time_utc == 0:
+        even = True
+        week_checked = 'четная'
     output = '⸻⸻⸻⸻⸻\n' + 'Группа: ' + group.upper() + '\n' \
         + 'День недели: ' + DAYS_RU[day_time_utc].capitalize() + '\n' + 'Неделя: ' + week_checked.capitalize() + '\n' \
         + '⸻⸻⸻⸻⸻\n'
     group = translit(group, language_code='ru', reversed=True)
 
-    if day_type == 'завтра':
-        if day_time_utc == 7:
-            day_of_week = DAYS_ENG[0]
-        else:
-            day_of_week = DAYS_ENG[day_time_utc]
-    else:
-        day_of_week = DAYS_ENG[day_time_utc]
+    day_of_week = DAYS_ENG[day_time_utc] if day_type == 'завтра' else DAYS_ENG[day_time_utc]
 
     responce = json.loads(await aiohttp_fetch(url=f'http://{RESTIP}:{RESTPORT}/schedule/?group={group}&even={even}&day={day_of_week}'))
     output += responce['schedule']
@@ -47,14 +49,18 @@ async def print_full_schedule(day_type: str, group: str) -> str:
     week_checked = await get_week()
     if week_checked == 'четная':
         if day_type == 'следующая неделя':
-            even = False
+            even = False 
+            week_checked = 'нечетная' 
         else:
             even = True
+            week_checked = 'четная' 
     else:
         if day_type == 'следующая неделя':
-            even = True
+            week_checked = 'четная'
+            even = True 
         else:
             even = False
+            week_checked = 'нечетная' 
 
     output = '⸻⸻⸻⸻⸻\n' + 'Группа: ' + group.upper() + '\n' \
         + 'Неделя: ' + week_checked.capitalize() + '\n' + '⸻⸻⸻⸻⸻\n'
