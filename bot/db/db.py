598c039fc729995cb6e9a769645af198fe606fc0
3xiced <3xiced@gmail.com>
2022-10-08 13:48:23 +0300

    Fix db seeding

--- bot/db/db.py
+++ bot/db/db.py
@@ -52,9 +52,25 @@ async def db_connect(user: str, password: str, name: str, host: str) -> asyncpg.
             )
             await sys_conn.close()
             connection: asyncpg.Connection = await asyncpg.connect(user=user, password=password, database=name, host=host)
-            with open('bot/db/dump.sql', 'r') as file:
-                logger.info("Seeding database")
-                await connection.execute(file)
+            logger.info("Seeding database...")
+            commands = ["CREATE TABLE IF NOT EXISTS users(user_id integer NOT NULL,date timestamp NOT NULL,command text NOT NULL);",
+                        "CREATE TABLE IF NOT EXISTS menu_buttons_table(user_id INT NOT NULL, button TEXT NOT NULL);",
+                        "CREATE TABLE IF NOT EXISTS config(user_id integer NOT NULL,keyboard_buttons text NOT NULL,is_writing boolean NOT NULL,button_to_write text NOT NULL);",
+                        "CREATE TABLE IF NOT EXISTS accesses(user_id integer NOT NULL,full_admin_panel BOOLEAN NOT NULL, semi_admin_panel BOOLEAN NOT NULL,headman_panel BOOLEAN NOT NULL,stream_group TEXT);",
+                        "INSERT INTO accesses VALUES(255632502, False, False, True, 'бвт2101');",
+                        "INSERT INTO accesses VALUES(228506651, False, False, True, 'бвт2102');",
+                        "INSERT INTO accesses VALUES(355129349, False, False, True, 'бвт2103');",
+                        "INSERT INTO accesses VALUES(116008591, False, False, True, 'бвт2104');",
+                        "INSERT INTO accesses VALUES(237079184, False, False, True, 'бвт2105');",
+                        "INSERT INTO accesses VALUES(210481885, True, True, True, 'бвт2103');",
+                        "INSERT INTO accesses VALUES(162956112, True, True, True, 'бвт2103');",
+                        "INSERT INTO accesses VALUES(213304238, False, True, False, NULL);",
+                        "INSERT INTO accesses VALUES(528860991, False, True, False, NULL);",
+                        "INSERT INTO accesses VALUES(278233695, False, True, False, NULL);"
+                        ]
+            for comd in commands:
+                logger.info("Executing: " + comd)
+                await connection.execute(comd)
             return connection
         except Exception as e:
             tries -= 1
